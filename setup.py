2c5ba634b152663bf75d1355e727d44445ddd8ed
Adrián Bernardi <adrian@silix.com.ar>
2025-06-09 18:06:10 -0300

    Move the reports to the report directory

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ setup(name='%s_%s' % (PREFIX, MODULE),
         ),
     package_data={
         'trytond.modules.%s' % MODULE: (info.get('xml', []) + [
-            'tryton.cfg', 'view/*.xml', 'locale/*.po', '*.fodt']),
+            'tryton.cfg', 'view/*.xml', 'locale/*.po', 'report/*.fodt']),
         },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
