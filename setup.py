e0fd2ea6d078cb31e673bf9adfd8db818f3d16f2
Adrián Bernardi <adrian@silix.com.ar>
2025-08-08 21:37:39 -0300

    Update tryton.cfg and setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,14 @@ import io
 import os
 import re
 from configparser import ConfigParser
-
 from setuptools import find_packages, setup
 
 MODULE = 'account_report_ar'
 PREFIX = 'trytonar'
-MODULE2PREFIX = {}
+MODULE2PREFIX = {
+    'party_ar': 'trytonar',
+    'account_invoice_ar': 'trytonar',
+    }
 
 
 def read(fname):
@@ -27,10 +29,7 @@ def read(fname):
 def get_require_version(name):
     if name in LINKS:
         return ''  # '%s @ %s' % (name, LINKS[name])
-    if minor_version % 2:
-        require = '%s >= %s.%s.dev0, < %s.%s'
-    else:
-        require = '%s >= %s.%s, < %s.%s'
+    require = '%s >= %s.%s, < %s.%s'
     require %= (name, major_version, minor_version,
         major_version, minor_version + 1)
     return require
@@ -51,17 +50,26 @@ url = 'https://github.com/tryton-ar/%s' % MODULE
 download_url = 'https://github.com/tryton-ar/%s/tree/%s.%s' % (
     MODULE, major_version, minor_version)
 
-LINKS = {}
+LINKS = {
+    'trytonar_party_ar': ('git+https://github.com/tryton-ar/'
+        'party_ar.git@%s.%s#egg=trytonar_party_ar-%s.%s' %
+        (major_version, minor_version, major_version, minor_version)),
+    'trytonar_account_invoice_ar': ('git+https://github.com/tryton-ar/'
+        'account_invoice_ar.git@%s.%s#egg=trytonar_account_invoice_ar-%s.%s' %
+        (major_version, minor_version, major_version, minor_version)),
+    }
 
-requires = []
+requires = ['unidecode >= 1.0.23']
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res)(\W|$)', dep):
         module_name = '%s_%s' % (MODULE2PREFIX.get(dep, 'trytond'), dep)
         requires.append(get_require_version(module_name))
-
 requires.append(get_require_version('trytond'))
 
 tests_require = [get_require_version('proteus')]
+for dep in info.get('extras_depend', []):
+    module_name = '%s_%s' % (MODULE2PREFIX.get(dep, 'trytond'), dep)
+    tests_require.append(get_require_version(module_name))
 
 setup(name='%s_%s' % (PREFIX, MODULE),
     version=version,
@@ -76,6 +84,7 @@ setup(name='%s_%s' % (PREFIX, MODULE),
         "Forum": 'https://www.tryton.org/forum',
         "Source Code": url,
         },
+    keywords='',
     package_dir={'trytond.modules.%s' % MODULE: '.'},
     packages=(
         ['trytond.modules.%s' % MODULE]
@@ -83,7 +92,8 @@ setup(name='%s_%s' % (PREFIX, MODULE),
         ),
     package_data={
         'trytond.modules.%s' % MODULE: (info.get('xml', []) + [
-            'tryton.cfg', 'view/*.xml', 'locale/*.po', 'report/*.fodt']),
+            'tryton.cfg', 'view/*.xml', 'locale/*.po', 'report/*.fodt',
+            'report/*.fods', 'tests/*.rst', 'tests/*.txt']),
         },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
